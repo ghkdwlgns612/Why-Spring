70e30c6304d5fb8fae8a56681fe8a8e9b1ca4b4b
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-07-23 12:45:17 +0900

    mvc이용

---
+++ spring/src/main/java/why/spring/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,28 @@
+package why.spring.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import why.spring.domain.member.Member;
+import why.spring.domain.member.MemberRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username,age);
+        memberRepository.save(member);
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member",member);
+        return mv;
+    }
+}
